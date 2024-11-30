fb02eda8ca78241a8bbcb25b7cccd2f876c63f1b
Shivranjan Kolvankar <shivranjan1689@gmail.com>
2024-11-30 23:55:31 +0530

    Parameters Fixed

--- src/model.py
+++ src/model.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(10, 10, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(10 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
@@ -14,7 +14,7 @@ class MNISTModel(nn.Module):
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
+        x = x.view(-1, 10 * 7 * 7)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x 
