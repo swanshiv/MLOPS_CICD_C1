60f6b93da70f989a989e9bc350714cbc8f0fa3ff
Shivranjan Kolvankar <shivranjan1689@gmail.com>
2024-12-01 10:43:46 +0530

    Model Parameters Changed again for Out of Bounds Problem

--- src/model.py
+++ src/model.py
@@ -4,17 +4,17 @@ import torch.nn as nn
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 9, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(9, 9, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(9 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 9)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(10, 10, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(10 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 9 * 7 * 7)
+        x = x.view(-1, 10 * 7 * 7)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x 
