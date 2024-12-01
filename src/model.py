c4ea03e986fc434d0ebcd7e3d7563f87ecec33eb
Shivranjan Kolvankar <shivranjan1689@gmail.com>
2024-12-01 13:51:35 +0530

    Model Parameters to be less than 25K

--- src/model.py
+++ src/model.py
@@ -4,17 +4,21 @@ import torch.nn as nn
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(10, 10, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(10 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(16 * 7 * 7, 64)
+        self.fc2 = nn.Linear(64, 10)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 10 * 7 * 7)
+        x = self.dropout(x)
+        x = x.view(-1, 16 * 7 * 7)
         x = self.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x 
